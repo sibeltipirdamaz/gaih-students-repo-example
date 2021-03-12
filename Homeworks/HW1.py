63195213577feb5e77c0a1881c2bc1433924d1df
sibeltipirdamaz <80275357+sibeltipirdamaz@users.noreply.github.com>
2021-03-12 08:51:05 +0300

    Update HW1.py

--- Homeworks/HW1.py
+++ Homeworks/HW1.py
@@ -1,5 +1,14 @@
 #Explain your work
+oddlist = list(range(1,50,2))
+evenlist = list(range(0,50,2))
+newlist = oddlist + evenlist
+newlist = [i*2 for i in newlist]
 
-#Question 1
-for x in range(a):
-	print(a)
+print (oddlist)
+print ("\n")
+print (evenlist)
+print ("\n")
+print (newlist)
+print ("\n")
+for i in newlist: 
+  print (type (i))
