cf6db82a13d3d674037105bf54d99b195b376b0d
sibeltipirdamaz <80275357+sibeltipirdamaz@users.noreply.github.com>
2021-03-12 09:11:49 +0300

    Update HW2.py

--- Homeworks/HW2.py
+++ Homeworks/HW2.py
@@ -1,5 +1,9 @@
-#Explain your work
-
-#Question 1
-for x in range(a):
-	print(a)
+first = {"category" : "finance", "experience" : "4 years", "position" : "assistant specialist" }
+second = {"category" : "marketing", "experience" : "10 years", "position" : "coordinator" }
+third = {"category" : "sales", "experience" : "2 years", "position" : "junior" }
+fourth = {"category" : "research", "experience" : "15 years", "position" : "senior coordinator" }
+fifth = {"category" : "human resources", "experience" : "6 years", "position" : "senior specialist" }
+all = {"category" : ["finance", "marketing", "sales", "research", "human resources" ] , 
+       "experience" : ["4 years", "10 years" , "2 years", "15 years", "6 years"] , 
+       "position" : ["assistant specialist", "coordinator", "junior", "senior coordinator", "senior specialist"] }
+all 
