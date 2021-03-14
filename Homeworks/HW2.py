e9b7bb1fd7340c96f3ea279dc4ac7b6cd410cf87
sibeltipirdamaz <80275357+sibeltipirdamaz@users.noreply.github.com>
2021-03-14 21:14:13 +0300

    Update HW2.py

--- Homeworks/HW2.py
+++ Homeworks/HW2.py
@@ -1,9 +1,21 @@
-first = {"category" : "finance", "experience" : "4 years", "position" : "assistant specialist" }
-second = {"category" : "marketing", "experience" : "10 years", "position" : "coordinator" }
-third = {"category" : "sales", "experience" : "2 years", "position" : "junior" }
-fourth = {"category" : "research", "experience" : "15 years", "position" : "senior coordinator" }
-fifth = {"category" : "human resources", "experience" : "6 years", "position" : "senior specialist" }
-all = {"category" : ["finance", "marketing", "sales", "research", "human resources" ] , 
-       "experience" : ["4 years", "10 years" , "2 years", "15 years", "6 years"] , 
-       "position" : ["assistant specialist", "coordinator", "junior", "senior coordinator", "senior specialist"] }
-all 
+first = {"name":"Sibel Tıpırdamaz","category" : "finance", "experience" : "4 years", "position" : "assistant specialist" }
+second = {"name":"Osman Yılmaz","category" : "marketing", "experience" : "10 years", "position" : "coordinator" }
+third = {"name":"Melisa Öztürk","category" : "sales", "experience" : "2 years", "position" : "junior" }
+fourth = {"name":"Süha Kaya","category" : "research", "experience" : "15 years", "position" : "senior coordinator" }
+fifth = {"name":"Toprak Deniz","category" : "human resources", "experience" : "6 years", "position" : "senior specialist" }
+
+all = {}
+
+all[0] = first;
+all[1] = second;
+all[2] = third;
+all[3] = fourth;
+all[4] =fifth;
+
+for i in all:
+  print(str(i+1)+". CV")
+  print("Name: "+ all[i]["name"])
+  print("Category: "+ all[i]["category"])
+  print("Experience: "+ all[i]["experience"])
+  print("Position: "+ all[i]["position"])
+  print("*")
