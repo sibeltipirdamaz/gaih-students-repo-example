0e642b560e3b60dc23e256af269091e0880438cd
sibeltipirdamaz <80275357+sibeltipirdamaz@users.noreply.github.com>
2021-03-14 22:06:46 +0300

    Update HW3.py

--- Homeworks/HW3.py
+++ Homeworks/HW3.py
@@ -1,5 +1,17 @@
-#Explain your work
+num=0
+d = {}
+list = [0.0,0.0,0.0,0.0,0.0]
+while num<5 : 
+  name = input ("Enter your name ")
+  midterm = int(input("What is your midterm grade? "))
+  project = int(input("What is your project grade? "))
+  final =  int(input("What is your final grade? "))  
+  passingGrade = midterm*0.3 + project*0.3 + final*0.4
+  d[name] = passingGrade
+  list[num] = passingGrade
+  num=num+1
 
-#Question 1
-for x in range(a):
-	print(a)
+print(list)
+list.sort()
+list.reverse()
+print(list)
